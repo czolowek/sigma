84c15300987ba0a2957f8783d98048621a57bd2a
czolowek <klutoi39@gmail.com>
2024-10-12 11:13:37 +0200

    26дз

---
+++ 25.py
@@ -0,0 +1,31 @@
+class women:
+    rost = "152"
+    color = "black"
+    run = False
+    age = "15"
+
+    def age(self):
+        self.age = print(f"вумене"'{age}'"лет")
+        
+    def runing(self):
+            self.run = True
+            print("вомен донть ланинг")
+   
+print(women.runing)
+print(women.age)
+
+
+class boys:
+    color = "white"
+    rost = "133"
+    run = True
+    age = "11"
+
+    def age(self):
+        self.age = print(f"бойу "'{age}'"лет")
+        
+    def runing(self):
+            self.run = True
+            print(" бой донть ланинг лол")
+print(boys.age)
+print(boys.runing)
--- доп.дз.py
+++ доп.дз.py
@@ -1,4 +1,7 @@
-text = ("аламагаламага сидела на табуретке")
-print(text)
-print(text[3])
-print(text[-1])
+# text = ("аламагаламага сидела на табуретке")
+# print(text)
+# print(text[3])
+# print(text[-1])
+
+dict = ("a,b,c")
+
