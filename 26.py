4af2e5718894fb5046b8b5476acdf3b34a8c3d53
czolowek <klutoi39@gmail.com>
2024-10-18 21:02:45 +0200

    ОБНОВА

---
+++ 26.py
@@ -0,0 +1,26 @@
+class Mynoratata:
+    def __init__(self):
+        self.brand = "PUBLIC AND FOR YOU "
+        self.fuel = "PRACTIK LOL"
+        self.temperature = 33
+
+    def change_temp(self, new_lala):
+        print(f"private kuku: {self.__tem = }")
+        self.tem = new_lala
+        print("privat not for you:  {self.__temp}")
+        print(f"bro its for you,not privat")
+
+
+
+    def _protected_metod(self):
+        print("PROTECTED METHOD AHAHA")
+
+
+    def _privat_metod(self):
+        print("THIS IS NOT FOR YOU")
+
+
+    def greenstart(self):
+        self._protected_metod()
+
+
